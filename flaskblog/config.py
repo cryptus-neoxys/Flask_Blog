5df163a5b4d9681a4df416d0e1f33f6d75abdf57
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-08-03 00:35:09 +0530

    config added

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,14 +1,17 @@
 import os
+import json
 
+with open('flaskblog/config.json') as config_file: 
+    config = json.load(config_file)
 
 
 class Config:
-	SECRET_KEY = os.environ.get('SECRET_KEY_FOR_FLASK_BLOG')
-	SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI_FOR_FLASK_BLOG')
+	SECRET_KEY = config.get('SECRET_KEY_FOR_FLASK_BLOG')
+	SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI_FOR_FLASK_BLOG')
 	MAIL_SERVER = 'smtp.gmail.com'
 	MAIL_PORT = '465'
 	MAIL_USE_TLS = False
 	MAIL_USE_SSL = True
-	MAIL_USERNAME = os.environ.get('EMAIL_USER')
-	MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+	MAIL_USERNAME = config.get('EMAIL_USER')
+	MAIL_PASSWORD = config.get('EMAIL_PASS')
 	MAIL_DEFAULT_SENDER = ('Dev form Flask Blog', 'dared8002@gmail.com')
